22190ac23acac13dffaa55ee14aa13d1253339eb
robertriceok <robertriceok@gmail.com>
2020-04-22 15:34:00 +0800

    修復

--- src/tw/eis/model/dao/IUtilDao.java
+++
@@ -1,7 +0,0 @@
-package tw.eis.model.dao;
-
-public interface IUtilDao {
-	public String createDeptList();
-	public String createTitleList();
-	public String createEmpList();
-}
--- src/tw/eis/model/service/IUtilService.java
+++
@@ -1,7 +0,0 @@
-package tw.eis.model.service;
-
-public interface IUtilService {
-	public String createDeptList();
-	public String createTitleList();
-	public String createEmpList();
-}
