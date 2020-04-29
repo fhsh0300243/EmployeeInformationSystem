23f1c7c8eb7bad69f48f30800b2871223b3eb861
GKK0430 <egham0110@gmail.com>
2020-04-29 17:23:20 +0800

    首頁提示功能添加

--- WebContent/WEB-INF/js/littleRemind.js
+++ WebContent/WEB-INF/js/littleRemind.js
@@ -3,11 +3,14 @@ $(document)
 				function() {
 					var oldDate = "";
 					var newDate = "";
-
+					var num1;
+					var num2;
+					var num3;
+				
+					
 					checkDate();
 					function checkDate() {
-						$
-								.ajax({
+						$.ajax({
 									url : "http://localhost:8080/EmployeeInformationSystem/checkDate",
 									type : "get",
 									cache : false,
@@ -16,21 +19,37 @@ $(document)
 										var rs2 = JSON.parse(rs);
 										oldDate = rs2[0].oldDate;
 										newDate = rs2[0].newDate;
-
-										oldDate = new Date(oldDate);
-										newDate = new Date(newDate);
-										if (((newDate - oldDate) / (1000 * 86400)) > 1) {
+										
 											if (oldDate == null) {
 												console.log();
 												$("#maintext").html("安安，新人");
 												querybullboard();
 												$('#myModal').modal();
 											} else {
+												oldjsDate = new Date(oldDate);
+												newjsDate = new Date(newDate);
+												
+//												if (((newDate - oldDate) / (1000 * 86400)) > 1) {
 												query();
 												querybullboard();
+												querysucess(oldDate,newDate);
+												
+												console.log("num1:"+num1);
+												console.log("num2:"+num2);
+												console.log("num3:"+num3);
+												
+												console.log("num3:"+num3)
+												if((num1==null || num1==0)
+												 &&(num2==null || num2==0)
+												 &&(num3==null || num3==0)
+												){
+													$("#maintext").html("沒有新消息!");
+												}
+												
+												
 												$('#myModal').modal();
+//												}
 											}
-										}
 									},
 									error : function(rs) {
 										console.log("error");
@@ -39,8 +58,7 @@ $(document)
 					}
 
 					function query() {
-						$
-								.ajax({
+						$.ajax({
 									url : "http://localhost:8080/EmployeeInformationSystem/queySingerPage",
 									type : "get",
 									cache : false,
@@ -52,16 +70,44 @@ $(document)
 										if (Num != 0) {
 											$("#modaltext")
 													.html(
-															"有"
-																	+ Num
-																	+ "個下屬的差旅費還沒被簽核!");
+															"有"+ Num+ "個下屬的差旅費申請尚未被簽核!");
 										}
+										num1=Num;
 									},
 									error : function(rs) {
 										alert("error");
 									}
 								})
 					}
+					
+					function querysucess(oldDate,newDate) {
+						$.ajax({
+									url : "http://localhost:8080/EmployeeInformationSystem/querysucess",
+									type : "get",
+									cache : false,
+									async : false,
+									dataType : "text",
+									data : {
+										"oldDate":oldDate,
+										"newDate":newDate
+									},
+									success : function(rs) {
+										console.log("sucess");
+										var Num = parseInt(rs);
+										if (Num != 0) {
+											$("#querysucess")
+													.html(
+															"你提出的"+Num+ "個差旅費申請已被簽核完成!");		
+										}
+										num2=Num;
+										
+									},
+									error : function(rs) {
+										alert("error");
+									}
+								})
+					}
+					
 
 					function querybullboard() {
 						$
@@ -70,12 +116,17 @@ $(document)
 									type : "get",
 									dataType : "text",
 									cache : false,
+									async : false,
 									success : function(rs) {
 										var Num = parseInt(rs);
 										if (Num != 0) {
 											$("#bullboardtext").html(
 													"有" + Num + "個新的布告欄通知!");
 										}
+										console.log("Num3"+Num)
+										num3=Num;
+										console.log("num3"+num3)
+										
 									},
 									error : function(rs) {
 										console.log("reflash error");
