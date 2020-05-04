92c9dc8997af7b38180a3e32fb32d4e76f5024a4
cx10612 <cx10612@gmail.com>
2020-05-04 13:06:37 +0800

    for windows login controller

--- src/tw/eis/controller/LoginController.java
+++ src/tw/eis/controller/LoginController.java
@@ -74,11 +74,11 @@ public class LoginController {
 		}
 		
 //      Windows AES加密		
-//		String encryptPwd=aes.parseByte2HexStr(aes.encrypt(userPassword)); 
+		String encryptPwd=aes.parseByte2HexStr(aes.encrypt(userPassword)); 
 
 
 //		Linux AES加密
-		String encryptPwd=password.encrypt("1234567890123456", userPassword);
+//		String encryptPwd=password.encrypt("1234567890123456", userPassword);
 //		System.out.println("cleartext:" + encryptPwd);
 		
 		List<Users> loginResult=uService.findUsers(userName, encryptPwd);
